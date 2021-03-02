fd100528aee0ab0139d76cd0800bfcfdfe47eb91
warakorn <boydoubleb@gmail.com>
2021-03-02 22:03:37 +0700

    add: multiplication table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [number, setNumber] = useState(0);
+
+  console.log(number);
+
+  const listArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(+e.target.value)}
+      />
+      <button className="button">Calculate</button>
+      <br />
+      <ul className="list">
+        {number !== 0
+          ? listArray.map((_, index) => (
+              <li key={index}>
+                {number} x {index} = {number * index}
+              </li>
+            ))
+          : null}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
